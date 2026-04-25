cb3c713b244575ba8ff971fa10dc08bf10f715d3
William <william.tocas.yanqui@gmail.com>
2026-04-24 19:51:32 -0500

    Agregado ejemplos de la semana 4

---
+++ s4_while.cs
@@ -0,0 +1,6 @@
+int i = 1; // inicializando la variable control
+while(i <= 100){
+    Console.WriteLine(i);
+    i++; // lo mismo que i = i + 1 // actualizando la variable control
+}
+Console.WriteLine("Fin del While");
